b6b06ddd3a7c8f719d43da009afe079ea1c31d08
Jakub Němec <Jakub.K.Nemec@gmail.com>
2020-03-31 11:33:31 +0200

    .

---
+++ SoučetAObvodČtverce-OOP/Obsah-Obvod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SoučetAObvodČtverce_OOP
+{
+    public class Obsah_Obvod
+    {
+
+    public void sum()
+        {
+            Console.Write("Your number:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Your second number:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double obvod = 2 * (num1 + num2);
+            double obsah = num1 * num2;
+
+            Console.WriteLine("Circuit is: {0}cm", obvod);
+            Console.WriteLine("Content is: {0}cm ", obsah);
+        }
+
+    }
+}
---
+++ SoučetAObvodČtverce-OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoučetAObvodČtverce_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obsah_Obvod class1 = new Obsah_Obvod();
+            class1.sum();
+
+            Console.ReadKey();
+        }
+    }
+}
